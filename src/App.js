d89c249455bae4b65401dea9fafbaffb4afac04b
Raghu Aryan <raguveer.bhopaly@gmail.com>
2021-12-24 18:13:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>We are integrating material ui in this react app.</p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
